3b876e4c27b7d3a9cdf3145c7b9a4ac0ce780f2e
Elvander <bart@jdsoft.be>
2012-08-18 10:53:30 +0200

    Initial commit

---
+++ public/template/application_list.js
@@ -0,0 +1,46 @@
+<div class="row-fluid">
+	<div class="span2"></div> 
+	<div class="span8">
+		<div class="well" style="margin-bottom: 0px; padding-bottom: 0px;">
+	   	<div class="row-fluid" >
+	   		<div class="span4">
+			   	<ul class="thumbnails">
+			   		<li>
+			          <a href="#" class="thumbnail">
+			            <img src="http://placehold.it/200x240" alt="">
+			          </a>
+			        </li>
+			   	</ul>
+	   		</div>
+	   		<div class="span8">
+		    	<h2>Fireplug Website <small>Commercial website for the fireplug app.</small></h2>
+				<p>
+				Environment: Stage   Play! Framework: 2.0.3
+				</p>
+				Server load
+				<div class="progress progress-striped progress-warning">
+				  <div class="bar" style="width: 35%;"></div>
+				</div>
+
+				Database Usage
+				<div class="progress progress-striped progress-success">
+				  <div class="bar" style="width: 20%;"></div>
+				</div>
+				
+				Storage
+				<div class="progress progress-striped ">
+				  <div class="bar" style="width: 60%;"></div>
+				</div>
+
+	   			<div class="form-actions">
+	            	<button type="submit" class="pull-right btn btn-primary">Open details</button>
+	            	<button class="btn btn-danger">Stop</button>
+	            	<button class="btn btn-success">Start</button>
+	            </div>
+	   		</div>
+	   	</div>
+	   	
+   	</div>
+	</div>
+
+</div>
